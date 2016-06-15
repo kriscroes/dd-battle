f6530097bf57ebbed6c4876df4ce29fc605d4fdc
kriscroes <k_croes@hotmail.com>
2016-06-15 17:00:57 -0400

    converted from sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,8 @@
 
 ActiveRecord::Schema.define(version: 20160419203622) do
 
-  create_table "final_positions", force: :cascade do |t|
-    t.text     "position"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "finalgroups", force: :cascade do |t|
     t.integer  "startup_id"
@@ -27,8 +24,8 @@ ActiveRecord::Schema.define(version: 20160419203622) do
     t.boolean  "iswinner"
   end
 
-  add_index "finalgroups", ["finalposition_id"], name: "index_finalgroups_on_finalposition_id"
-  add_index "finalgroups", ["startup_id"], name: "index_finalgroups_on_startup_id"
+  add_index "finalgroups", ["finalposition_id"], name: "index_finalgroups_on_finalposition_id", using: :btree
+  add_index "finalgroups", ["startup_id"], name: "index_finalgroups_on_startup_id", using: :btree
 
   create_table "finalpositions", force: :cascade do |t|
     t.text     "name"
@@ -43,12 +40,6 @@ ActiveRecord::Schema.define(version: 20160419203622) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "qualification_positions", force: :cascade do |t|
-    t.text     "position"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "qualificationgroups", force: :cascade do |t|
     t.integer  "startup_id"
     t.integer  "qualificationposition_id"
@@ -56,8 +47,8 @@ ActiveRecord::Schema.define(version: 20160419203622) do
     t.datetime "updated_at",               null: false
   end
 
-  add_index "qualificationgroups", ["qualificationposition_id"], name: "index_qualificationgroups_on_qualificationposition_id"
-  add_index "qualificationgroups", ["startup_id"], name: "index_qualificationgroups_on_startup_id"
+  add_index "qualificationgroups", ["qualificationposition_id"], name: "index_qualificationgroups_on_qualificationposition_id", using: :btree
+  add_index "qualificationgroups", ["startup_id"], name: "index_qualificationgroups_on_startup_id", using: :btree
 
   create_table "qualificationpositions", force: :cascade do |t|
     t.text     "name"
@@ -72,60 +63,14 @@ ActiveRecord::Schema.define(version: 20160419203622) do
     t.datetime "updated_at",           null: false
   end
 
-  add_index "semifinalgroups", ["semifinalposition_id"], name: "index_semifinalgroups_on_semifinalposition_id"
-  add_index "semifinalgroups", ["startup_id"], name: "index_semifinalgroups_on_startup_id"
+  add_index "semifinalgroups", ["semifinalposition_id"], name: "index_semifinalgroups_on_semifinalposition_id", using: :btree
+  add_index "semifinalgroups", ["startup_id"], name: "index_semifinalgroups_on_startup_id", using: :btree
 
   create_table "semifinalpositions", force: :cascade do |t|
     t.text     "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "semifinals", force: :cascade do |t|
-    t.text     "position"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "startup_finals", force: :cascade do |t|
-    t.integer  "final_positions_id"
-    t.integer  "startup_id"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-  end
-
-  add_index "startup_finals", ["final_positions_id"], name: "index_startup_finals_on_final_positions_id"
-  add_index "startup_finals", ["startup_id"], name: "index_startup_finals_on_startup_id"
-
-  create_table "startup_qualifications", force: :cascade do |t|
-    t.integer  "qualification_positions_id"
-    t.integer  "startup_id"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-  end
-
-  add_index "startup_qualifications", ["qualification_positions_id"], name: "index_startup_qualifications_on_qualification_positions_id"
-  add_index "startup_qualifications", ["startup_id"], name: "index_startup_qualifications_on_startup_id"
-
-  create_table "startup_semifinals", force: :cascade do |t|
-    t.integer  "semifinal_id"
-    t.integer  "startup_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
-
-  add_index "startup_semifinals", ["semifinal_id"], name: "index_startup_semifinals_on_semifinal_id"
-  add_index "startup_semifinals", ["startup_id"], name: "index_startup_semifinals_on_startup_id"
-
-  create_table "startup_winners", force: :cascade do |t|
-    t.integer  "winner_id"
-    t.integer  "startup_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "startup_winners", ["startup_id"], name: "index_startup_winners_on_startup_id"
-  add_index "startup_winners", ["winner_id"], name: "index_startup_winners_on_winner_id"
 
   create_table "startupparticipants", force: :cascade do |t|
     t.integer  "startup_id"
@@ -135,8 +80,8 @@ ActiveRecord::Schema.define(version: 20160419203622) do
     t.datetime "updated_at",     null: false
   end
 
-  add_index "startupparticipants", ["participant_id"], name: "index_startupparticipants_on_participant_id"
-  add_index "startupparticipants", ["startup_id"], name: "index_startupparticipants_on_startup_id"
+  add_index "startupparticipants", ["participant_id"], name: "index_startupparticipants_on_participant_id", using: :btree
+  add_index "startupparticipants", ["startup_id"], name: "index_startupparticipants_on_startup_id", using: :btree
 
   create_table "startups", force: :cascade do |t|
     t.string   "name"
@@ -146,7 +91,7 @@ ActiveRecord::Schema.define(version: 20160419203622) do
     t.datetime "updated_at",  null: false
   end
 
-  add_index "startups", ["track_id"], name: "index_startups_on_track_id"
+  add_index "startups", ["track_id"], name: "index_startups_on_track_id", using: :btree
 
   create_table "tracks", force: :cascade do |t|
     t.string   "name"
@@ -154,10 +99,13 @@ ActiveRecord::Schema.define(version: 20160419203622) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "winners", force: :cascade do |t|
-    t.text     "position"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+  add_foreign_key "finalgroups", "finalpositions"
+  add_foreign_key "finalgroups", "startups"
+  add_foreign_key "qualificationgroups", "qualificationpositions"
+  add_foreign_key "qualificationgroups", "startups"
+  add_foreign_key "semifinalgroups", "semifinalpositions"
+  add_foreign_key "semifinalgroups", "startups"
+  add_foreign_key "startupparticipants", "participants"
+  add_foreign_key "startupparticipants", "startups"
+  add_foreign_key "startups", "tracks"
 end
